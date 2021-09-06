6a769fbc19a92551be00158293735fe84a85e4e3
Asta Kat <astakat@gmail.com>
2021-09-06 14:14:32 +0200

    first commit

---
+++ src/components/List.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+class List extends React.Component {
+    render(){
+        return(
+            <ul>
+                <li>Thee</li>
+                <li>Soep</li>
+                <li>Brood</li>
+            </ul>
+        )
+        
+    }
+}
+
+export default List;
